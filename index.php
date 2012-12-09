30deb37dd51315cd43463d34d7758ade63be07fd
zoltax <filipkulig@gmail.com>
2012-12-10 00:09:16 +0100

    Project scaffolfing

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<meta charset="UTF-8" />
+		<title>FTInfo</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	</head>
+	<body>
+		<div id="container" >
+			<header>
+			</header>
+			<!-- <div id="promo"></div> -->
+
+			<div id="menu">
+				<ul>
+					<li><a href="index.php?page=main">Strona główna</a></li>
+					<li><a href="index.php?page=portfolio">Portfolio</a></li>
+					<li><a href="index.php?page=offer">Oferta</a></li>
+					<li><a href="index.php?page=pricing">Cennik</a></li>
+					<li><a href="index.php?page=contact">Kontakt</a></li>
+				</ul>
+			</div>
+
+			<div id="content">
+				<div class="span11">
+					<div class="span3">
+						<div class="p_img">
+							<div class="img"> </div>
+						</div>
+						<div class="p_title">
+							<strong>This is a title</strong>
+						</div>
+						<div class="p_desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam justo non odio lacinia consectetur.
+						</div>
+					</div>
+					<div class="span3">
+						<div class="p_img">
+							<div class="img"> </div>
+						</div>
+						<div class="p_title">
+							<strong>This is a title</strong>
+						</div>
+						<div class="p_desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam justo non odio lacinia consectetur.
+						</div>
+					</div>
+					<div class="span3">
+						<div class="p_img">
+							<div class="img"> </div>
+						</div>
+						<div class="p_title">
+							<strong>This is a title</strong>
+						</div>
+						<div class="p_desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam justo non odio lacinia consectetur.
+						</div>
+					</div>
+				</div>
+				<div class="span11">
+					<div class="span3">
+						<div class="p_img">
+							<div class="img"> </div>
+						</div>
+						<div class="p_title">
+							<strong>This is a title</strong>
+						</div>
+						<div class="p_desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam justo non odio lacinia consectetur.
+						</div>
+					</div>
+					<div class="span3">
+						<div class="p_img">
+							<div class="img"> </div>
+						</div>
+						<div class="p_title">
+							<strong>This is a title</strong>
+						</div>
+						<div class="p_desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam justo non odio lacinia consectetur.
+						</div>
+					</div>
+					<div class="span3">
+						<div class="p_img">
+							<div class="img"> </div>
+						</div>
+						<div class="p_title">
+							<strong>This is a title</strong>
+						</div>
+						<div class="p_desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam justo non odio lacinia consectetur.
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="space3"></div>
+			<div class="space3"></div>
+
+		</div>
+		<footer>
+			<div id="footer_content">
+				Filip Kulig FTInfo 
+			</div>
+		</footer>
+	</body>
+
+</html>
